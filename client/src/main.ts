861b3bad974a991de3f3a5bbfa15eb05b6a82270
Jacob Chapman <7908073+chapmanjacobd@users.noreply.github.com>
2022-05-28 01:10:00 -0500

    index main todo

--- client/src/main.ts
+++ client/src/main.ts
@@ -20,15 +20,23 @@ if (devMode) API_domain = "http://127.0.0.1:8000/v1";
 
 
 alasql('CREATE localStorage DATABASE IF NOT EXISTS RuntimeDB')
+alasql('ATTACH localStorage DATABASE RuntimeDB')
+alasql('SET AUTOCOMMIT ON')
+alasql('CREATE TABLE IF NOT EXISTS videos');
 
 window.alasql = alasql
 window.Alpine = Alpine
 window.LiteYTEmbed = LiteYTEmbed
 
 window.app = {
-  fetchPlaylistInfo: async function (playlist: string) {
-    if (playlist.length > 5)
-      console.log('fetching new playlist');
+  alpineUpdate: function (evtName: string) {
+    const event = new Event(evtName)
+    window.dispatchEvent(event)
+  },
+  fetchPlaylist: async function (playlist: string) {
+    if (playlist.length < 5) return;
+
+    console.log('fetching new playlist');
 
     fetch(`${API_domain}?playlist=` + playlist)
       .then(response => response.json())
@@ -37,8 +45,8 @@ window.app = {
         alasql('SET AUTOCOMMIT ON')
 
         alasql(`DELETE from videos where webpage_url = "${data.webpage_url}"`)
-        alasql('CREATE TABLE IF NOT EXISTS videos');
         alasql(`INSERT INTO videos SELECT * FROM ?`, [[data]])
+        app.alpineUpdate('tableListUpdate')
       })
   },
   parseYTid: function (url: string) {
@@ -65,8 +73,8 @@ window.app = {
 }
 
 if (devMode) {
-  app.fetchPlaylistInfo('https://www.youtube.com/playlist?list=PL8A83124F1D79BD4F')
-  app.fetchPlaylistInfo('https://www.youtube.com/playlist?list=PLAskVVPnzWMBa_Jw9IbCm_uyjI8oawr-W')
+  app.fetchPlaylist('https://www.youtube.com/playlist?list=PL8A83124F1D79BD4F')
+  app.fetchPlaylist('https://www.youtube.com/playlist?list=PLAskVVPnzWMBa_Jw9IbCm_uyjI8oawr-W')
 }
 
 Alpine.start()
